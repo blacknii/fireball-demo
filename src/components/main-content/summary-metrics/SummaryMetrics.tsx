ab0540dd0393e38bdba582074c818b33efc7ad2b
blacknii <42908977+blacknii@users.noreply.github.com>
2023-08-17 16:42:55 +0200

    .

--- src/components/main-content/summary-metrics/SummaryMetrics.tsx
+++ src/components/main-content/summary-metrics/SummaryMetrics.tsx
@@ -1,6 +1,37 @@
 import { Box } from "@mui/material";
+import { Meteorite } from "../../../shared/meteorite.model";
 
-const SummaryMetrics = () => {
+const SummaryMetrics = ({
+  meteorites,
+  loading,
+}: {
+  meteorites: Meteorite[];
+  loading: boolean;
+}) => {
+  let numberOfStrikes = 0;
+  let totalMass = 0;
+
+  meteorites.forEach((meteorite) => {
+    numberOfStrikes++;
+    totalMass += meteorite.mass !== "NaN" ? parseFloat(meteorite.mass) : 0;
+    console.log({ totalMass });
+  });
+
+  if (loading) {
+    return (
+      <Box
+        style={{
+          backgroundColor: "white",
+          padding: 2,
+          flexGrow: 1,
+          width: "100%",
+        }}
+      >
+        <p>loading...</p>
+      </Box>
+    );
+  }
+
   return (
     <Box
       style={{
@@ -10,7 +41,8 @@ const SummaryMetrics = () => {
         width: "100%",
       }}
     >
-      <p>Summary Metrics</p>
+      <p>Total number of strikes: {numberOfStrikes}</p>
+      <p>Average mass: {(totalMass / numberOfStrikes).toFixed(2)}</p>
     </Box>
   );
 };
